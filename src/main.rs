8a77413b74011f733d84df4725c6b1b06bc6fa53
Athul Muralidhar <athul@keana.io>
2022-09-18 17:48:13 +0200

    added readme and boiler plate

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// the main idea of the project is to port nasa's astrobee into rust
+// this is aimed as a prototype for spacecraft core which will go into the space crafts themselves
+// so that they can be deployed in space
+// main reference repo here: git@github.com:AthulMuralidhar/core-space.git
+//main doc guide is here: https://nasa.github.io/astrobee/html/astrobee.html#autotoc_md513
+
+
+fn main() {
+    println!("Hello, world!");
+}
